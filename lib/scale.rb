a8be74fcf35edcb8108651e98eb556a749a6458a
Ari Russo <ari.russo@gmail.com>
2014-04-05 13:34:37 -0400

    comment

--- lib/scale.rb
+++ lib/scale.rb
@@ -3,7 +3,7 @@ require "scale/destination"
 require "scale/scheme"
 require "scale/source"
 
-# A Ruby helper for scaling Arrays, Ranges and Sets
+# A Ruby helper for scaling numbers
 module Scale
 
   VERSION = "0.1"
