6d26467897a9b45ed880bbfc8419aa8da593f59f
Ari Russo <ari.russo@gmail.com>
2014-04-05 14:27:37 -0400

    more shortcut methods

--- lib/scale/scheme.rb
+++ lib/scale/scheme.rb
@@ -4,7 +4,29 @@ module Scale
   # @param [Numeric] input
   # @return [Scale::Scheme]
   def self.transform(input)
-    Scheme.new(:input => input)
+    Scheme.new.scale(input)
+  end
+
+  # Build a scaling scheme starting with the given source
+  # @param [::Enumerable] source
+  # @return [Scale::Scheme]
+  def self.from(source)
+    Scheme.new.from(source)
+  end
+
+  # Build a scaling scheme starting with the given destination
+  # @param [::Enumerable] destination  
+  # @return [Scale::Scheme]
+  def self.to(destination)
+    Scheme.new.to(destination)
+  end
+
+  # Build a scaling scheme starting with the given source and destination
+  # @param [::Enumerable] source
+  # @param [::Enumerable] destination
+  # @return [Scale::Scheme]
+  def self.using(source, destination)
+    Scheme.new.using(:source => source, :destination => destination)
   end
 
   # Describes a particular scaling scenario
@@ -13,8 +35,8 @@ module Scale
     attr_reader :destination, :input, :source
 
     # @param [Hash] options
-    # @option options [Scale::Destination] :destination The destination for this scaling scenario
-    # @option options [Scale::Source] :source The source for this scaling scenario
+    # @option options [::Enumerable] :destination The destination for this scaling scenario
+    # @option options [::Enumerable] :source The source for this scaling scenario
     def initialize(options = {})
       @input = options[:input]
       @source = Source.new(options[:source]) unless options[:source].nil?
@@ -25,39 +47,59 @@ module Scale
     # has all of its needed properties, the scaled value will be returned.  Otherwise
     # this method will return the updated Scheme object.
     #
-    # @param [Scale::Source] source
-    # @return [Fixnum, Scale::Scheme]
+    # @param [::Enumerable] source
+    # @return [Numeric, Scale::Scheme]
     def from(source)
       @source = Source.new(source)
-      if @input.nil? || @destination.nil?
-        self
-      else
-        @destination.scale(@input, @source)
-      end
+      scale? ? result : self
     end
 
     # Set the destination for this scaling scenario.  If on calling this method, the 
     # scenario has all of its needed properties, the scaled value will be returned.  
     # Otherwise this method will return the updated Scheme object.
     #
-    # @param [Scale::Destination] destination
-    # @return [Fixnum, Scale::Scheme]
+    # @param [::Enumerable] destination
+    # @return [Numeric, Scale::Scheme]
     def to(destination)
       @destination = Destination.new(destination)
-      if @input.nil? || @source.nil?
-        self
-      else
-        @destination.scale(@input, @source) 
-      end
+      scale? ? result : self
     end
 
-    # Set both the source and destination on this scheme
-    # @param [Scale::Source] source
-    # @param [Scale::Destination] destination
+    # Set both the source and destination on this scheme. If on calling this method, the 
+    # scenario has all of its needed properties, the scaled value will be returned.  
+    # Otherwise this method will return the updated Scheme object.
+    #
+    # @param [::Enumerable] source
+    # @param [::Enumerable] destination
+    # @return [Numeric, Scale::Scheme]
     def using(source, destination)
       @source = Source.new(source)
       @destination = Destination.new(destination)
-      @destination.scale(@input, @source)
+      scale? ? result : self
+    end
+
+
+    # Set the input of this scaling scenario.  If on calling this method, the 
+    # scenario has all of its needed properties, the scaled value will be returned.  
+    # Otherwise this method will return the updated Scheme object.
+    # 
+    # @param [Numeric] number
+    # @return [Numeric, Scale::Scheme]
+    def scale(number)
+      @input = number
+      scale? ? result : self
+    end
+
+    # Scan this scaling scenario be run?
+    # @return [Boolean] Whether this scaling scenario can be run
+    def scale?
+      !@input.nil? && !@source.nil? && !@destination.nil?
+    end
+
+    # Get the result of this scaling scenario
+    # @return [Numeric, nil]
+    def result
+      @result ||= @destination.scale(@input, @source)
     end
 
   end
